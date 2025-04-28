01d9af521629b6f5c0f2de2b660ffa8a319d3b5d
IIMasterXII <erich.showalter@gmail.com>
2025-04-28 03:09:32 -0700

    Created the starting MERN stack code for Render.

---
+++ client/src/main.tsx
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App.jsx';
+import Home from './pages/Home';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
