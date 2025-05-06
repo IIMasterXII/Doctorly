59bd24d89a147f0ac44ff1cab87631944b80d602
IIMasterXII <erich.showalter@gmail.com>
2025-05-06 04:27:14 -0700

    revert server changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,9 @@
 import express from 'express';
 import db from './config/connection.js'
-import path from 'node:path';
-import type { Request, Response } from 'express';
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Note: Importing the express middleware from @apollo/server/express4
 // allows us to use the express middleware with Apollo Server.
@@ -39,10 +32,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static('../client/dist'));
 
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get('*', (_req, res) => {
+      res.sendFile('../client/dist/index.html');
     });
   }
 
